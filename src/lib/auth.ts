0558c0afe6d975778aa8d43768729bced66f1794
harishanandan555 <harish_a@bullbox.in>
2024-11-06 17:03:50 +0530

    code

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,10 +3,8 @@ import { AuthOptions } from "next-auth";
 import Google from "next-auth/providers/google";
 
 import { db } from "@/lib/db";
-
+import {google} from "../../data.json"
 // redirect_uri: "http://localhost:3000/api/auth/callback/google"
-let google_client_id = "622095554406-32i6saoa7sn60bu32n33f4um21ep2i65.apps.googleusercontent.com"
-let google_client_sec = "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"
 
 let NEXTAUTH_SEC = "7dBS+GeL22Ilenx6t0DsDqoKnJuzWoUQPbcll+UPxjs="        // openssl rand -base64 32
 let NEXTAUTH_JWT_SEC = "iaw2FR2ZhSNkjxR5KrLe42IvbRMTmhV5r2ZcVA3QrN8= "    // openssl rand -base64 32
@@ -15,8 +13,8 @@ export const authOptions = {
 	adapter: PrismaAdapter(db),
 	providers: [
 		Google({
-			clientId: google_client_id!,
-			clientSecret: google_client_sec!,
+			clientId: google.google_client_id!,
+			clientSecret: google.google_client_sec!,
 		}),
 	],
 	session: { strategy: "jwt" },
